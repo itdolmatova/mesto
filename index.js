14446c2d864a40600f172dd27eece98760890315
Anastasya D <itdolmatova@gmail.com>
2022-02-15 23:42:43 +0300

    <feat>: formatting

--- index.js
+++ index.js
@@ -12,34 +12,34 @@ let formElement = document.querySelector(".edit-form");
 let nameInput = formElement.querySelector(".edit-form__input_ctrl_name");
 let jobInput = formElement.querySelector(".edit-form__input_ctrl_job");
 
-function popupOpened () {
+function popupOpened() {
   popup.classList.add('popup_opened');
 }
 
-editButton.addEventListener ('click', popupOpened);
+editButton.addEventListener('click', popupOpened);
 
-function popupClosed () {
+function popupClosed() {
   popup.classList.remove('popup_opened');
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
 }
 
-closeButton.addEventListener ('click', popupClosed);
+closeButton.addEventListener('click', popupClosed);
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
+function formSubmitHandler(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
 
-    // Получите значение полей jobInput и nameInput из свойства value
-    // Выберите элементы, куда должны быть вставлены значения полей
-    
-    // Вставьте новые значения с помощью textContent
-    profileTitle.textContent = nameInput.value;
-    profileSubtitle.textContent = jobInput.value;
-    popup.classList.remove('popup_opened');
+  // Получите значение полей jobInput и nameInput из свойства value
+  // Выберите элементы, куда должны быть вставлены значения полей
+
+  // Вставьте новые значения с помощью textContent
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  popup.classList.remove('popup_opened');
 }
 
 // Прикрепляем обработчик к форме:
