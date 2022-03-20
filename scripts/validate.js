d430aa909316ae781961c0ac78c98fdfe2ff8cb5
Anastasya D <itdolmatova@gmail.com>
2022-03-20 19:22:23 +0300

    <refact>: add validation for inputs

---
+++ scripts/validate.js
@@ -0,0 +1,69 @@
+
+const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+        return !inputElement.validity.valid;
+    });
+}
+
+const toggleButtonState = (validationParams, inputList, buttonElement) => {
+  if (hasInvalidInput (inputList)) {
+    buttonElement.classList.add(validationParams.inactiveButtonClass);
+  } else {
+    buttonElement.classList.remove(validationParams.inactiveButtonClass);  
+  }
+}
+
+
+
+const showInputError = (validationParams, formElement, inputElement, validationMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(validationParams.inputErrorClass);
+    errorElement.textContent = validationMessage;
+    errorElement.classList.add(validationParams.errorClass);
+};
+
+const hideInputError = (validationParams, formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(validationParams.inputErrorClass);
+    errorElement.classList.remove(validationParams.errorClass);
+    errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) { 
+        showInputError(validationParams, formElement, inputElement, inputElement.validationMessage);
+    } else {
+        hideInputError(validationParams, formElement, inputElement);
+    }
+}
+
+function setEventListeners(validationParams, formElement) {
+  const inputList = Array.from(formElement.querySelectorAll(validationParams.inputSelector));
+  const buttonElement = formElement.querySelector(validationParams.submitButtonSelector);
+
+  toggleButtonState(validationParams, inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', function () {
+        checkInputValidity(validationParams, formElement, inputElement);
+        toggleButtonState(validationParams, inputList, buttonElement);
+      });
+  });
+}
+
+function enableValidation(validationParams) {
+  const formList = Array.from(document.querySelectorAll(validationParams.formSelector));
+
+  formList.forEach((formElement) => {setEventListeners(validationParams, formElement)});
+}
+
+
+
+enableValidation({
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__button',
+    inactiveButtonClass: 'popup__button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visible'
+  });
